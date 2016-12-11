6955d158eb90717e4d9db370e214bb633ace9bcd
skylocke <mathematical.fiction@gmail.com>
2016-12-10 21:29:56 -0800

    init

---
+++ js/main.js
@@ -0,0 +1,33 @@
+AFRAME.registerComponent('scale-on-click', {
+  schema: {
+    to: {default: '2 2 2'}
+  },
+  init: function () {
+    var data = this.data;
+    this.el.addEventListener('click', function () {
+      this.setAttribute('scale', data.to);
+    });
+  }
+});
+
+var sphere = document.getElementsByTagName("a-sphere")[0];
+
+var scene = document.getElementsByTagName("a-scene")[0];
+
+var testSphere = document.createElement("a-sphere");
+
+var pacman = document.createElement("a-cylinder");
+pacman.setAttribute("position", "-3 2 -6");
+pacman.setAttribute("color", "yellow");
+pacman.setAttribute("rotation", "90 0 0");
+
+scene.appendChild(pacman);
+
+var XYZ = [-2, 2, -4];
+
+var bloop = setInterval(function() {
+  xyzString = XYZ.join(' ');
+  pacman.setAttribute("position", xyzString);
+  XYZ[0]++
+  console.log(XYZ);
+}, 1000);
