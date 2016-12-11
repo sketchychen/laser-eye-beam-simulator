f5147e8812b0a6b647dc88de8b9168b807fae18b
skylocke <mathematical.fiction@gmail.com>
2016-12-10 22:37:37 -0800

    yay basic mathing

--- js/enemies.js
+++ js/enemies.js
@@ -1,7 +1,34 @@
 function vector(xyz1, xyz2) {
-  // xyz1 and xyz2 are coordinates (x, y, z)
-  // in the form of [x, y, z]
-  var dx = xyz1[0]-xyz2[0];
-  var dy = xyz1[1]-xyz2[1];
-  var dz = xyz1[2]-xyz2[2];
+  /*
+  parameters are arrays of length 3
+  returns an array of length 3
+  calculates vector from point xyz1 to xyz2
+  */
+  var dx = (xyz1[0]-xyz2[0]);
+  var dy = (xyz1[1]-xyz2[1]);
+  var dz = (xyz1[2]-xyz2[2]);
+  return [dx, dy, dz];
+}
+
+function magnitude(xyz1, xyz2) {
+  /*
+  parameters are arrays of length 3
+  returns a scalar float
+  calculates vector from point xyz1 to xyz2
+  */
+  var deltaXYZ = vector(xyz1, xyz2);
+  var magXYZ = Math.sqrt(deltaXYZ[0]**2 + deltaXYZ[1]**2 + deltaXYZ[2]**2);
+  return magXYZ;
+}
+
+function unitVector(xyz1, xyz2) {
+  /*
+  parameters are arrays of length 3
+  returns an array of length 3
+  calculates unit vector from point xyz1 to xyz2
+  */
+  var vec = vector(xyz1, xyz2);
+  var mag = magnitude(xyz1, xyz2);
+  var unit = [vec[0]/mag, vec[1]/mag, vec[2]/mag];
+  return unit;
 }
